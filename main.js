330b0c3320e8a1341236c4cba285287176f20646
Timothy Andrew Gass <timothyagass@gmail.com>
2015-10-26 09:02:20 -0500

    much better

--- main.js
+++ main.js
@@ -3,101 +3,7 @@ var downKeys = "";
 var clicked = false;
 
 function dotheThing(thing){
-  var numberArray = [];
-  var operatorArray = [];
-  var lastTerm = 0;
-  var answer = 0;
-  var operated = [];
-  for(x = 0; x < thing.length; x++){
-    if((thing.charAt(x) === "/") || (thing.charAt(x) === "*") || (thing.charAt(x) === "-") || (thing.charAt(x) === "+")){
-      numberArray.push(thing.slice(lastTerm, (x)));
-      operatorArray.push(thing.slice(x, x+1));
-      lastTerm = x+1;
-    }
-  }
-
-  numberArray.push(thing.slice(lastTerm));
-
-  for (var i = 0; i < operatorArray.length; i++) {
-      if(answer != 0){
-        if (operated.indexOf(numberArray[i]) != -1) {
-          if(operatorArray[i] === "*"){
-            answer = answer*Number(numberArray[i+1]);
-            operated.push(numberArray[i]);
-            operated.push(numberArray[i+1]);
-          }
-          else if(operatorArray[i] === "/"){
-            answer = answer/Number(numberArray[i+1]);
-            operated.push(numberArray[i]);
-            operated.push(numberArray[i+1]);
-          }
-        }
-        else if(operated.indexOf(numberArray[i+1]) != -1){
-          if(operatorArray[i] === "*"){
-            answer = answer*Number(numberArray[i+1]);
-            operated.push(numberArray[i]);
-            operated.push(numberArray[i+1]);
-          }
-          else if(operatorArray[i] === "/"){
-            answer = Number(numberArray[i+1])/answer;
-            operated.push(numberArray[i]);
-            operated.push(numberArray[i+1]);
-          }
-        }
-      }
-
-
-      else if(answer == 0){
-        if(operatorArray[i] === "*"){
-          answer += Number(numberArray[i])*Number(numberArray[i+1]);
-          operated.push(numberArray[i]);
-          operated.push(numberArray[i+1]);
-        }
-        else if(operatorArray[i] === "/"){
-          answer += Number(numberArray[i])/Number(numberArray[i+1]);
-          operated.push(numberArray[i]);
-          operated.push(numberArray[i+1]);
-        }
-      }
-    }
-
-
-  for (var i = 0; i < operatorArray.length; i++) {
-    if(answer != 0){
-      if (operated.indexOf(numberArray[i]) != -1) {
-        if(operatorArray[i] === "+"){
-          answer = answer+Number(numberArray[i+1]);
-        }
-        else if(operatorArray[i] === "-"){
-          answer = answer-Number(numberArray[i+1]);
-        }
-      }
-
-      else if (operated.indexOf(numberArray[i+1]) != -1){
-        if(operatorArray[i] === "+"){
-          answer = answer+Number(numberArray[i]);
-        }
-        else if(operatorArray[i] === "-"){
-          answer = Number(numberArray[i]) - answer;
-        }
-      }
-    }
-    else if (answer == 0) {
-      if(operatorArray[i] === "+"){
-        answer += Number(numberArray[i]) + Number(numberArray[i+1]);
-      }
-      else if(operatorArray[i] === "-"){
-        answer += Number(numberArray[i]) - Number(numberArray[i+1]);
-      }
-    }
-  }
-
-  if(operatorArray.length === 0){
-    answer = thing;
-    clicked = false;
-  }
-
-  return answer;
+  return eval(thing);
 };
 
 function charles(yolo) {
@@ -113,17 +19,10 @@ function charles(yolo) {
   }
 
   else if (keyCode === 55) {
-    if(clicked === false){
       document.getElementById('view').value += "7";
-    }
-    else {
-      document.getElementById('view').value = "7";
-      clicked = false;
-    }
   }
 
   else if (keyCode === 56) {
-    if(clicked === false){
       if(downKeys.length === 0){
         document.getElementById('view').value += "8";
       }
@@ -131,147 +30,58 @@ function charles(yolo) {
         document.getElementById('view').value += "*";
         downKeys = "";
       }
-    }
-    else {
-      if(downKeys.length === 0){
-        document.getElementById('view').value = "8";
-      }
-      else {
-        document.getElementById('view').value = "*";
-        downKeys = "";
-      }
-      clicked = false;
-    }
   }
 
   else if (keyCode === 57) {
-    if(clicked === false){
       document.getElementById('view').value += "9";
-    }
-    else {
-      document.getElementById('view').value = "9";
-      clicked = false;
-    }
   }
 
   else if (keyCode === 88) {
-    if(clicked === false){
       document.getElementById('view').value += "*";
-    }
-    else {
-      document.getElementById('view').value = "*";
-      clicked = false;
-    }
   }
 
   else if (keyCode === 52) {
-    if(clicked === false){
       document.getElementById('view').value += "4";
-    }
-    else {
-      document.getElementById('view').value = "4";
-      clicked = false;
-    }
   }
 
   else if (keyCode === 53) {
-    if(clicked === false){
       document.getElementById('view').value += "5";
-    }
-    else {
-      document.getElementById('view').value = "5";
-      clicked = false;
-    }
   }
 
   else if (keyCode === 54) {
-    if(clicked === false){
       document.getElementById('view').value += "6";
-    }
-    else {
-      document.getElementById('view').value = "6";
-      clicked = false;
-    }
   }
 
   else if (keyCode === 191) {
-    if(clicked === false){
       document.getElementById('view').value += "/";
-    }
-    else {
-      document.getElementById('view').value = "/";
-      clicked = false;
-    }
   }
 
   else if (keyCode === 49) {
-    if(clicked === false){
       document.getElementById('view').value += "1";
-    }
-    else {
-      document.getElementById('view').value = "1";
-      clicked = false;
-    }
   }
 
   else if (keyCode === 50) {
-    if (clicked === false) {
       document.getElementById('view').value += "2";
-    }
-    else {
-      document.getElementById('view').value = "2";
-      clicked = false;
-    }
   }
 
   else if (keyCode === 51) {
-    if(clicked === false){
       document.getElementById('view').value += "3";
-  }
-    else {
-      document.getElementById('view').value = "3";
-      clicked = false;
-    }
   }
 
   else if (keyCode === 189) {
-    if(clicked === false){
       document.getElementById('view').value += "-";
-  }
-    else {
-      document.getElementById('view').value = "-";
-      clicked = false;
-    }
   }
 
   else if (keyCode === 48) {
-    if (clicked === false) {
       document.getElementById('view').value += "0";
-  }
-    else {
-      document.getElementById('view').value = "0";
-      clicked = false;
-    }
   }
 
   else if (keyCode ===  190) {
-    if (clicked === false) {
       document.getElementById('view').value += ".";
-    }
-    else {
-      document.getElementById('view').value = ".";
-      clicked = false;
-    }
   }
 
   else if (keyCode === 8) {
-    if(clicked === false){
       document.getElementById('view').value = document.getElementById('view').value.slice(0, (document.getElementById('view').value.length -1));
-    }
-    else {
-      document.getElementById('view').value = "";
-      clicked = false;
-    }
   }
 
   else if (keyCode === 18) {
@@ -279,7 +89,6 @@ function charles(yolo) {
   }
 
   else if (keyCode === 187) {
-    if( clicked === false){
       if (downKeys.length === 0) {
         document.getElementById('view').value = dotheThing(document.getElementById('view').value);
       }
@@ -288,17 +97,8 @@ function charles(yolo) {
         downKeys = "";
       }
     }
-    else {
-      if (downKeys.length === 0) {
-        document.getElementById('view').value = dotheThing(document.getElementById('view').value);
-      }
-      else{
-        document.getElementById('view').value = "+";
-        downKeys = "";
-      }
-    }
   }
-}
+
 
 function text(yolo) {
 
@@ -317,12 +117,10 @@ function text(yolo) {
 
   else if (keyCode === 187) {
     if (downKeys.length === 0) {
-      document.getElementById('view').value = dotheThing(document.getElementById('view').value).slice(0, ((dotheThing(document.getElementById('view').value)).length));
+      document.getElementById('view').value = dotheThing(document.getElementById('view').value);
     }
     else{
-      setTimeout(function(){
       downKeys = "";
-      }, 1);
     }
   }
 
@@ -335,13 +133,7 @@ function text(yolo) {
 document.addEventListener("DOMContentLoaded", function(){
   function clickHandler(event){
     event.addEventListener("click", function(){
-      if (clicked === false) {
       document.getElementById('view').value += event.value;
-    }
-    else {
-      document.getElementById('view').value = event.value;
-      clicked = false;
-    }
     }, false);
   };
 
